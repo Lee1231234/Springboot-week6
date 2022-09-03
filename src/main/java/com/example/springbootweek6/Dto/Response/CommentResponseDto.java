062bffd7bbe30e4f6359a28fab792c8c3946434d
Lee1231234 <qscgyd1629@naver.com>
2022-09-03 21:27:18 +0900

    post comment 생성 수정 삭제

---
+++ src/main/java/com/example/springbootweek6/Dto/Response/CommentResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.springbootweek6.Dto.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentResponseDto {
+    private Long id;
+    private String author;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+}
