24ade9bc24e892f8d6bc7109562362eb713b12b6
Lee1231234 <qscgyd1629@naver.com>
2022-09-06 11:56:41 +0900

    에러제거중

--- src/test/java/com/example/springbootweek6/SpringbootWeek6ApplicationTests.java
+++ src/test/java/com/example/springbootweek6/SpringbootWeek6ApplicationTests.java
@@ -6,6 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootWeek6ApplicationTests {
 
+
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
+
     @Test
     void contextLoads() {
     }
